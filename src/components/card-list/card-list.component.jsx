7755713036a6f5fb0c49214da4a82b9efa8ebf98
Unknown <raphman100@gmail.com>
2020-08-16 18:53:02 -0400

    Add all mnster-rolodex files

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,9 +7,9 @@ import './card-list.styles.css';
 export const CardList = props => (
 	<div className='card-list'>
 		{
-			props.monsters.map(monster => (
-				<Card key={monster.id} monster={monster} />
-			))
+			props.monsters.map( monster => (
+				<Card key={ monster.id } monster={ monster }/>
+			) )
 		}
 	</div>
 );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,8 +4,8 @@ import './card.styles.css';
 
 export const Card = props => (
 	<div className='card-container'>
-		<img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
-		<h3>{props.monster.name}</h3>
-		<p>{props.monster.email}</p>
+		<img alt='monster' src={ `https://robohash.org/${ props.monster.id }?set=set2&size=180x180` }/>
+		<h3>{ props.monster.name }</h3>
+		<p>{ props.monster.email }</p>
 	</div>
 );
